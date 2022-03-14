201a6090f505cb0699c31b33a8888c2fdd7cc900
Carmen Cogianu <carmen.cogianu@gmail.com>
2022-03-14 19:21:32 +0200

    created modal and faq section

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
+const openModal = document.getElementsByClassName("open-modal");
+const closeModal = document.getElementsByClassName("close-modal");
+const modalContainer = document.getElementById("modal-container");
+
+openModal[0].addEventListener('click', () => {
+    modalContainer.classList.add('show');
+});
+openModal[1].addEventListener('click', () => {
+    modalContainer.classList.add('show');
+});
+
+closeModal[0].addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+}); 
+
